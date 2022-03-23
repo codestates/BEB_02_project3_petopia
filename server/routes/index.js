d0f67abb6193b61906cf70be02783c05ec6a4a07
seungok2 <seung1560@gmail.com>
2022-03-23 10:53:08 +0900

    Server comment & like push

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const create = require('./create.js')
+const like = require('./like.js');
+const comment = require('./comment.js');
 
 router.use('/create', create);
+router.use('/like', like);
+router.use('/comment', comment);
+
 
 module.exports = router;
