be54a21117e0f8366462c3ea1a7318547bfeaa48
seungok2 <seung1560@gmail.com>
2022-03-23 11:00:49 +0900

    Server comment & like push

---
+++ server/services/likeService.js
@@ -0,0 +1,17 @@
+const like = require("../models/like.js")
+
+const insertLike = async(likeInfo) => {
+    const {postId, walletAddress} = likeInfo;
+    try {
+        const newLike = new like({post_id:postId, wallet_address:walletAddress});
+        await newLike.save();
+        return newLike;
+    } catch (error) {
+        throw Error(error);
+    }
+}
+
+
+module.exports = {
+    insertLike
+}
