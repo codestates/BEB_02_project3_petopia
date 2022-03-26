a968fe2bd93e2c6014dfa8c252ece41a77d27383
joocool <jcode7472@gmail.com>
2022-03-26 13:37:54 +0900

    fear:[server] add follow

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,12 @@ const create = require('./create.js');
 const user = require('./user.js');
 const contract = require('./contract.js');
 const hospital = require('./hospital.js');
+const follow = require('./follow.js')
 
 router.use('/user', user);
 router.use('/create', create);
 router.use('/contract', contract);
 router.use('/hospital', hospital);
+router.use('/follow', follow);
 
 module.exports = router;
