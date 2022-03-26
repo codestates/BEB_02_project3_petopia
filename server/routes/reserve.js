0b44f6932988adab1f3a802d5812ecf3ad827999
joocool <jcode7472@gmail.com>
2022-03-26 17:47:54 +0900

    fix:typo

--- server/routes/reserve.js
+++ server/routes/reserve.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const controller = require('../controllers/reserveController.js');
 
 router.post('/', controller.insertReserve);
-router.post('/:id', controller.getReserveList);
+router.post('/getReserveList', controller.getReserveList);
 
 module.exports = router;
