9b44998ec93333c5a86cbf095faf70391d28466f
joocool <jcode7472@gmail.com>
2022-03-30 00:02:09 +0900

    feat:prevent duplicated reply input

---
+++ client/src/components/Reply.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const Reply = ({commentId, replyDelete}) => {
+    const [replies, setReplies] = useState([]);
+
+    useEffect(() => {
+        loadReplies();
+    }, []);
+    
+    const loadReplies = async() =>{
+        await axios.get(`http://localhost:4000/reply/${commentId}`)
+        .then((res) => {
+            setReplies(res.data.data);
+        });
+    }
+
+    return(
+        <div className="replies">
+            {
+                replies.map((reply) => {
+                    return (
+                        <div key={reply._id} className="reply-wrapper" id={reply._id}>
+                            <span>└</span>
+                            <img className="rounded-circle" src={reply.user.profile_image} alt="profile" width="45"/>
+                            <div className="d-flex flex-column flex-wrap ml-2"><span class="font-weight-bold">{reply.user.user_name}</span></div>
+                            <p className="replyUser">{reply.contents}</p>
+                            <div className="post-time">
+                                <span>${reply.reply_date.split('T')[0]}</span>
+                            </div>
+                            <button id="btn_${reply._id}" data-user={reply.user._id} data-id={reply._id} onClick={(e)=>replyDelete(e.target)}> delete </button>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+};
+
+export default Reply;
